ac1965412db86c8a4f55e882ea7ff69b512f0db2
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-11-07 12:12:31 -0300

    :sparkles: Envio de e-mail

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,12 @@
 // Selecionar a Seção About
 const about = document.querySelector('#about');
 
+// Selecionar o formulário
+const formulario = document.querySelector('#formulario');
+
+// Expressão Regular para validação de e-mail
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
 // Função para buscar os dados no GitHub
 async function getApiGithub(){
 
@@ -60,6 +66,55 @@ async function getApiGithub(){
         console.error(error);
     }
 }
+
+// Função de envio e validação do formulário
+formulario.addEventListener('submit', function(event) {
+    
+    // Impedir o envio automático do formulário
+    event.preventDefault();
+
+    // Validação do campo nome
+    const campoNome = document.querySelector('#nome');
+    const txtNome = document.querySelector('#txtNome');
+
+    // Nome precisa ter no minimo 3 caracteres
+    if(campoNome.value.length < 3){
+        txtNome.innerHTML = 'O Nome deve ter no mínimo 3 caracteres.';
+        campoNome.focus();
+        return;
+    }else{
+        txtNome.innerHTML = '';
+    }
+
+    // Validação do campo e-mail
+    const campoEmail = document.querySelector('#email');
+    const txtEmail = document.querySelector('#txtEmail');
+
+    // Verifica se o e-mail é válido
+    if(!campoEmail.value.match(emailRegex)){
+        txtEmail.innerHTML = 'Digite um e-mail válido.';
+        campoEmail.focus();
+        return;
+    }else{
+        txtEmail.innerHTML = '';
+    }
+
+     // Validação do campo assunto
+    const campoAssunto = document.querySelector('#assunto');
+    const txtAssunto = document.querySelector('#txtAssunto');
+
+    // Assunto precisa ter no minimo 5 caracteres
+    if(campoAssunto.value.length < 5){
+        txtAssunto.innerHTML = 'O Assunto deve ter no mínimo 5 caracteres.';
+        campoAssunto.focus();
+        return;
+    }else{
+        txtAssunto.innerHTML = '';
+    }
+
+    // Se passou por todas as validações, envia o formulário
+    formulario.submit();
+})
 
 // Chamar a função getAPIGithub()
 
